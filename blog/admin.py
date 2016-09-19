e2e8d069cacf89f921c0776dddf8681d2f83b242
Gary Ditsch <garyditsch@gmail.com>
2016-09-19 14:23:05 -0400

    adding filtering and dates

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'get_absolute_url', 'is_published')
+
+admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
 from .models import Post
+
 
 # Create your views here.
 
 def index(request):
 
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lt=timezone.now())
 
     context = {
         "posts": posts
@@ -15,7 +18,7 @@ def index(request):
 
 
 def post(request, id):
-    post = Post.objects.get(pk=id)
+    post = get_object_or_404(Post, pk=id, published_date__lt=timezone.now())
 
     context = {
         "post":post
